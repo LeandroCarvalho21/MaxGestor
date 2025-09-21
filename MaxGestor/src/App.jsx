c121d8eb4319c7270cf6f71eab5a0e153dea9bed
LeandroCarvalho21 <leandrocnovais@gmail.com>
2025-09-21 20:48:11 -0300

    Criando tela de Login

--- MaxGestor/src/App.jsx
+++ MaxGestor/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './componentes/login/login'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <Login></Login>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -33,3 +37,4 @@ function App() {
 }
 
 export default App
+
---
+++ MaxGestor/src/componentes/login/Login.jsx
@@ -0,0 +1,20 @@
+import '../login/Login.css'
+
+function Login() {
+
+
+    return (
+
+        <>
+
+            <h1>Leandro</h1>
+
+        </>
+
+
+    )
+
+}
+
+
+export default Login
